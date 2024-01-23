ea27f6d7b7c6e853d65f65fcc889a0b9e079cd71
Paul Theby <paultheby@gmail.com>
2024-01-22 19:15:04 -0600

    Exercise 4

---
+++ Exercise4.sql
@@ -0,0 +1,12 @@
+CREATE TABLE books(
+BookID int,
+Title text,
+Author text,
+Price float
+);
+INSERT INTO books
+VALUES(1, 'The Great Gatsby', 'F. Scott Fitzgerald', 10.99),
+(2, '1984', 'George Orwell', 8.99),
+(3, 'The Catcher in the Rye', 'J. D. Salinger', 7.99);
+
+SELECT * FROM  Books
